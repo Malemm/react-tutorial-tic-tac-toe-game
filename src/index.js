61a80a3ab1e9583d09c0161f45f5064ce88c7070
unknown <malemlink@gmail.com>
2022-12-14 01:12:51 +0530

    title change

--- src/index.js
+++ src/index.js
@@ -3,10 +3,5 @@ import ReactDOM from "react-dom";
 
 import Game from "./Game"
 
-// ReactDOM.render(
-//     <Game />,
-//     document.querySelector("#root")
-// );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
